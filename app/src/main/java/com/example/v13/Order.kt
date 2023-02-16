6ff4bd079a8ebf984a41389bdb3b45455ecf488c
Gaurav Srivastava <52217208+gs1719@users.noreply.github.com>
2023-02-17 01:16:30 +0530

    another way of view binding

--- app/src/main/java/com/example/v13/Order.kt
+++ app/src/main/java/com/example/v13/Order.kt
@@ -5,19 +5,17 @@ import android.os.Bundle
 import com.example.v13.databinding.ActivityOrderBinding
 
 class Order : AppCompatActivity() {
-    private lateinit var binding: ActivityOrderBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
-        binding = ActivityOrderBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+
+        val binding = ActivityOrderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val orderOfCustomer = intent.getStringExtra(MainActivity.KEY)
 
         binding.textViewOrder.text=orderOfCustomer
 
     }
+
 }
